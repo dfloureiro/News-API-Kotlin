1e2e6c499af6de9ee8029f3698b0d55828856520
Diogo Loureiro <diogofialholoureiro@gmail.com>
2018-02-18 21:13:53 +0000

    added java example; added cacheDir arg; fixed null issues with requests;

--- app/src/main/java/dfl/com/newsapikotlin/MainActivity.kt
+++ app/src/main/java/dfl/com/newsapikotlin/MainActivity.kt
@@ -9,42 +9,50 @@ import dfl.com.newsapikotin.enums.Country
 import dfl.com.newsapikotin.enums.Language
 import dfl.com.newsapikotin.enums.SortBy
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val newsApi = NewsApi("")
+        val newsApi = NewsApi("", cacheDir)
 
-        newsApi.getSources(category = Category.GENERAL, language = Language.EN, country = Country.US)
+        compositeDisposable.add(newsApi.getSources(category = Category.GENERAL, language = Language.EN, country = Country.US)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .flatMapIterable { sources -> sources.sources }
                 .subscribe({ source -> Log.d("source", source.name) },
-                        { t -> Log.d("getSources error", t.message) })
+                        { t -> Log.d("getSources error", t.message) }))
 
-        newsApi.getEverything(q = "bitcoin", sources = "bbc-news", domains = null, from =  "2018-01-01", to = "2018-02-13", language = Language.EN, sortBy = SortBy.POPULARITY, pageSize = 20, page = 1)
+        compositeDisposable.add(newsApi.getEverything(q = "bitcoin", sources = "bbc-news", domains = null, from = "2018-01-01", to = "2018-02-13", language = Language.EN, sortBy = SortBy.POPULARITY, pageSize = 20, page = 1)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .flatMapIterable { articles -> articles.articles }
                 .subscribe({ article -> Log.d("getEverything article", article.title) },
-                        { t -> Log.d("getEverything error", t.message) })
+                        { t -> Log.d("getEverything error", t.message) }))
 
-        newsApi.getTopHeadlines(category = Category.GENERAL, country = Country.US, q = "trump", pageSize = 20, page = 1)
+        compositeDisposable.add(newsApi.getTopHeadlines(category = Category.GENERAL, country = Country.US, q = "trump", pageSize = 20, page = 1)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .flatMapIterable { articles -> articles.articles }
                 .subscribe({ article -> Log.d("getTopHead CC article", article.title) },
-                        { t -> Log.d("getTopHeadlines error", t.message) })
+                        { t -> Log.d("getTopHeadlines error", t.message) }))
 
-        newsApi.getTopHeadlines(sources = "bbc-news", q = "trump", pageSize = 20, page = 1)
+        compositeDisposable.add(newsApi.getTopHeadlines(sources = "bbc-news", q = "trump", pageSize = 20, page = 1)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .flatMapIterable { articles -> articles.articles }
                 .subscribe({ article -> Log.d("getTopHead S article", article.title) },
-                        { t -> Log.d("getTopHeadlines error", t.message) })
+                        { t -> Log.d("getTopHeadlines error", t.message) }))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 }
--- newsapikotin/src/main/java/dfl/com/newsapikotin/NetworkModule.kt
+++ newsapikotin/src/main/java/dfl/com/newsapikotin/NetworkModule.kt
@@ -1,13 +1,13 @@
 package dfl.com.newsapikotin
 
+import okhttp3.Cache
 import okhttp3.CacheControl
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
-import okhttp3.Cache
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -17,10 +17,10 @@ internal class NetworkModule {
 
     private val url = "https://newsapi.org/"
 
-    fun create(apiKey : String, cacheMaxAgeSeconds : Int, cacheMaxSize : Long, readTimeOutSeconds : Long, writeTimeoutSeconds : Long): NewsApiService {
+    fun create(apiKey: String, cacheDir: File, cacheMaxAgeSeconds: Int, cacheMaxSize: Long, readTimeOutSeconds: Long, writeTimeoutSeconds: Long): NewsApiService {
 
         val retrofit = Retrofit.Builder()
-                .client(createOkHttpClient(apiKey,cacheMaxAgeSeconds,cacheMaxSize,readTimeOutSeconds,writeTimeoutSeconds))
+                .client(createOkHttpClient(apiKey, cacheDir, cacheMaxAgeSeconds, cacheMaxSize, readTimeOutSeconds, writeTimeoutSeconds))
                 .addCallAdapterFactory(
                         RxJava2CallAdapterFactory.create())
                 .addConverterFactory(
@@ -31,15 +31,16 @@ internal class NetworkModule {
         return retrofit.create(NewsApiService::class.java)
     }
 
-    private fun createOkHttpClient(apiKey : String, cacheMaxAgeSeconds : Int, cacheMaxSize : Long, readTimeOutSeconds : Long, writeTimeoutSeconds : Long): OkHttpClient {
+    private fun createOkHttpClient(apiKey: String, cacheDir: File, cacheMaxAgeSeconds: Int, cacheMaxSize: Long, readTimeOutSeconds: Long, writeTimeoutSeconds: Long): OkHttpClient {
         val okHttpClient = OkHttpClient.Builder()
+                .cache(Cache(cacheDir, cacheMaxSize))
                 .addNetworkInterceptor({ chain ->
-                    val response = chain.proceed(chain.request())
                     val cacheControl = CacheControl.Builder().maxAge(cacheMaxAgeSeconds, TimeUnit.SECONDS)
                             .build()
-                    response.newBuilder()
-                            .header("Cache-Control", cacheControl.toString())
+                    val request = chain.request().newBuilder()
+                            .addHeader("Cache-Control", cacheControl.toString())
                             .build()
+                    chain.proceed(request)
                 })
                 .addInterceptor({ chain ->
                     val request = chain.request().newBuilder().addHeader("X-Api-Key", apiKey).build()
@@ -47,7 +48,6 @@ internal class NetworkModule {
                 })
                 .readTimeout(readTimeOutSeconds, TimeUnit.SECONDS)
                 .writeTimeout(writeTimeoutSeconds, TimeUnit.SECONDS)
-                .cache(Cache(File("/"), cacheMaxSize))
 
         return okHttpClient.build()
     }
